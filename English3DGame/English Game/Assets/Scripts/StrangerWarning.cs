4c0d3aa943b6e1448353e5ae21bdf97dad964001
Sulaiman M <sulaiman@Sulaimans-MacBook-Pro.local>
2019-04-17 00:57:11 +0100

    uploading assets before working on project

---
+++ English3DGame/English Game/Assets/Scripts/StrangerWarning.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrangerWarning : MonoBehaviour
+{
+    //TODO make the child's interaction with strangers pop up a warning message
+    //TODO try to do this with a trigger or interaction with only the people that that have this script (maybe use tag).
+    //TODO make the reply choose from a couple of sentences at random, so the response isnt always the same. 
+
+
+}
